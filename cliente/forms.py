2ed8a0cc398579ad54296e5facfa6dbf55c6f0dc
fabiocosin <fabiocosin@gmail.com>
2021-11-22 18:51:39 -0300

    Atualização_22.11.21

--- cliente/forms.py
+++ cliente/forms.py
@@ -3,6 +3,11 @@ from django.forms import fields, models
 from .models import cliente
 
 class ClienteForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type":"date"}
+        )
+    )
     class Meta:
         model = cliente
-        fields = ['nome_completo', 'data_nascimento', ]
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
